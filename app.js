60ff2c1fbf3fbbbf6eff487a846c8641f7d6d5bf
Saimun <saimun4u@gmail.com>
2021-05-23 23:48:56 -0500

    set-up javascript function and event listeners

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+//Function for Calculating Results
+
+function calculateResults(e){
+
+    console.log('Calculating...')
+
+    e.preventDefault();
+}
