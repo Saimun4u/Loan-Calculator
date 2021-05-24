46c5341562026bb40b7995a7e9a1d582cc163c33
Saimun <saimun4u@gmail.com>
2021-05-24 01:43:00 -0500

    Create error message for not entering loan figures

--- app.js
+++ app.js
@@ -6,5 +6,66 @@ function calculateResults(e){
 
     console.log('Calculating...')
 
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+    const monthlyPayment = document.getElementById('monthly-payment');
+    const totalPayment = document.getElementById('total-payment');
+    const totalInterest = document.getElementById('total-interest');
+
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Compute monthly payments
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal*x*calculatedInterest)/(x-1);
+
+    if (isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    }else{
+        showError('Please check your numbers')
+    }
+
     e.preventDefault();
 }
+
+function showError(error){
+
+    //Create Error Div
+
+    const errorDiv = document.createElement('div');
+
+    //Select DOM elements
+
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    //Add alert class 
+
+    errorDiv.className = 'alert alert-danger';
+
+    //Create Text node and append to the div
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    //Insert Text Node Before Heading
+
+    card.insertBefore(errorDiv, heading);
+
+    //Remove showError Div after 3 sec
+
+    setTimeout(clearError, 3000);
+
+}
+
+function clearError(){
+    document.querySelector('.alert').remove()
+}
+
+
+
+
